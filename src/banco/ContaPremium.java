82aa92ee5d24493ee19b00f019c4cefc65c0d1f4
Douglas Silva <douglasitpro@gmail.com>
2026-04-20 18:31:30 -0300

    Test ContaPremium: calcular IOF com base no saldo atual da contaPremium
    simulando cashback - creditar Bonus anual

--- src/banco/ContaPremium.java
+++ src/banco/ContaPremium.java
@@ -16,5 +16,24 @@ public class ContaPremium extends ContaBancaria implements Tributavel{
         this.bonusAnual = 0;
     }
 
+    /**
+     * Calcula IOF com base no saldo atual
+     */
+    @Override
+    public double calcularImposto() {
+        return saldo * ALIQUOTA_IOF;
+    }
+
+    /**
+     * Crédito de bônus anual (cachback simulado)
+     */
+    public void creditarBonus(double percentualDoSaldo){
+        bonusAnual = saldo * (percentualDoSaldo / 100.0);
+        saldo += bonusAnual;
+        System.out.printf("[BÔNUS PREMIUM] R$ %.2f creditados (%.1f%% do saldo).%n", bonusAnual, percentualDoSaldo);
+
+    }
+
+
 
 }
